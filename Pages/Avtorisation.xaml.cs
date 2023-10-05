2704c280aa83f30934343f46fe313b4ff7863c8e
user10 <89997083+MustafaAlibekov@users.noreply.github.com>
2023-10-05 13:06:11 +0300

    opa

---
+++ Pages/Avtorisation.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sport.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Avtorisation.xaml
+    /// </summary>
+    public partial class Avtorisation : Page
+    {
+        public Avtorisation()
+        {
+            InitializeComponent();
+        }
+
+        private void Enter(object sender, RoutedEventArgs e)
+        {
+            if(TxbLogin.Text == "1" && PsbParol.Password == "1")
+            Classes.manager.MainFrame.Navigate(new Pages.Admin());
+        }
+    }
+}
